2880053daeaf30c103624c331da840167ca86933
Toki1117 <dmsq2017@outlook.com>
2019-05-05 16:23:56 -0700

    adding js files

---
+++ js/capitulo1.js
@@ -0,0 +1,52 @@
+// CAPITULO 1
+export default cap1 = Object.create(Object.prototype,
+  {
+    ejercicios:{
+      value: Object.create(Object.prototype, { 
+				ejercicio1: {
+          value: Object.create(Object.prototype, {
+            url: {
+              value: "ejercicio1.zip"
+            },
+            img: {
+              value: "imagen.jpg"
+            }
+          })
+        },
+        ejercicio2: {
+					value: Object.create(Object.prototype, {
+            url: {
+              value: "ejercicio2.zip"
+            },
+            img: {
+              value: "imagen.jpg"
+            }
+          })
+				},
+      })
+    },
+    ejemplos :{
+      value: Object.create(Object.prototype, { 
+				ejemplo1: {
+					value: Object.create(Object.prototype, {
+            url: {
+              value: "ejemplo1.zip"
+            },
+            img: {
+              value: "imagen.jpg"
+            }
+          })
+        },
+        ejemplo2: {
+					value: Object.create(Object.prototype, {
+            url: {
+              value: "ejemplo2.zip"
+            },
+            img: {
+              value: "imagen.jpg"
+            }
+          })
+				},
+      })
+    },
+  });
